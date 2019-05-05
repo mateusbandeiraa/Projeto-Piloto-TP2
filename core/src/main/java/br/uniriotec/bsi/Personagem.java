8428577aa53ad5112f85fdd7365d7ad261fa49c1
mateusbandeiraa <mateusbandeiraa@gmail.com>
2019-05-05 15:11:03 -0300

    Sugestões de melhorias

--- core/src/main/java/br/uniriotec/bsi/Personagem.java
+++ core/src/main/java/br/uniriotec/bsi/Personagem.java
@@ -32,5 +32,27 @@ public class Personagem {
 		} else if (Gdx.input.isKeyPressed(Keys.DOWN)) {
 			sprite.setY(sprite.getY() + 4f);
 		}
+		
+		/*
+		 * @formatter:off
+		 * TODO:
+		 * 	### Alterar a direção do personagem para apontar em direção ao movimento ###
+		 * 		
+		 * 		Para espelhar a imagem de um sprite, utilize o método:
+		 * 
+		 * 		sprite.flip(boolean x, boolean y);
+		 * 
+		 * 		Se o primeiro argumento for true, o sprite será espelhado horizontalmente.
+		 * 		Se o segundo argumento for true, o sprite será espelhado verticalmente.
+		 * 		Se os dois argumentos fore true, o sprite será espelhado nos dois eixos.
+		 * 
+		 * 		Pode ser útil checar se um sprite já está espelhado antes de fazer a modificação.
+		 * 		Para isso, utilize os métodos:
+		 * 
+		 * 		sprite.isFlipX(); (retorna true se o sprite já está espelhado horizontalmente).
+		 * 		sprite.isFlipY(); (retorna true se o sprite já está espelhado verticalmente).
+		 * 
+		 * @formatter:on
+		 */
 	}
 }
--- core/src/main/java/br/uniriotec/bsi/ProjetoPilotoTP2.java
+++ core/src/main/java/br/uniriotec/bsi/ProjetoPilotoTP2.java
@@ -17,7 +17,7 @@ public class ProjetoPilotoTP2 extends BasicGame {
 	public void initialise() {
 		mapa = new TiledMap(Gdx.files.internal("mapa.tmx"));
 		TiledMap.STRICT_LAYER_VISIBILITY = true;
-		
+
 		jogador = new Personagem(new Texture("tankDesert.png"), 128f, 0, 32f, 32f);
 	}
 
@@ -35,6 +35,25 @@ public class ProjetoPilotoTP2 extends BasicGame {
 	public void render(Graphics g) {
 		g.drawString("Olá mundo!", 16f, 16f);
 		mapa.draw(g, 0, 0);
+		/*
+		 * @formatter:off
+		 * TODO:
+		 * 	### Renderizar o mapa no centro da tela ###
+		 * 
+		 * 		DICA: Para calcular a posição (x, y) que o mapa deverá ser renderizado, as
+		 * 		informações necessárias são:
+		 * 			1) Largura e Altura da tela;
+		 * 			2) Largura e Altura do mapa;
+		 * 
+		 * 			Para obter as dimensões da tela, os seguintes métodos podem ser usados:
+		 * 				g.getWindowWidth();
+		 *				g.getWindowHeight();
+		 *
+		 * 		A posição (x, y) para renderizar o mapa no centro da janela é: 
+		 * 			x = (larguraDaJanela - larguraDoMapa) / 2
+		 * 			y = (alturaDaJanela - alturaDoMapa) / 2
+		 * @formatter:on
+		 */
 		jogador.render(g);
 	}
 }
