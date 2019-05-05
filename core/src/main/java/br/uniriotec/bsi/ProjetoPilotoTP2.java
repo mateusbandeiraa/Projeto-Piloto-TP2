87c4a4b761cb6f5ec39d40f76db6a49981da729a
mateusbandeiraa <mateusbandeiraa@gmail.com>
2019-05-05 14:42:54 -0300

    Renderizar o mapa

--- core/src/main/java/br/uniriotec/bsi/ProjetoPilotoTP2.java
+++ core/src/main/java/br/uniriotec/bsi/ProjetoPilotoTP2.java
@@ -2,32 +2,34 @@ package br.uniriotec.bsi;
 
 import org.mini2Dx.core.game.BasicGame;
 import org.mini2Dx.core.graphics.Graphics;
+import org.mini2Dx.tiled.TiledMap;
 
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
 
 public class ProjetoPilotoTP2 extends BasicGame {
 	public static final String GAME_IDENTIFIER = "br.uniriotec.bsi.PPTP2";
 
-	private Texture texture;
-	
+	private TiledMap mapa;
+
 	@Override
-    public void initialise() {
-    	texture = new Texture("bsi.png");
-    }
-    
-    @Override
-    public void update(float delta) {
-    
-    }
-    
-    @Override
-    public void interpolate(float alpha) {
-    
-    }
-    
-    @Override
-    public void render(Graphics g) {
-		g.drawTexture(texture, 16f, 16f);
-		g.drawString("Olá mundo!", 16f, 245f);
-    }
+	public void initialise() {
+		mapa = new TiledMap(Gdx.files.internal("mapa.tmx"));
+		TiledMap.STRICT_LAYER_VISIBILITY = true;
+	}
+
+	@Override
+	public void update(float delta) {
+
+	}
+
+	@Override
+	public void interpolate(float alpha) {
+
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.drawString("Olá mundo!", 16f, 16f);
+		mapa.draw(g, 0, 0);
+	}
 }
